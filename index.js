845d574ba21f286eb4c577349a705a8f0ab53217
nohman20 <nohman20@gmail.com>
2016-07-31 18:07:11 -0700

    Fixes and simplifies logic in takeSquare and boardStatus

--- index.js
+++ index.js
@@ -2,47 +2,43 @@ window.onload = function () {
   let winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]]
   let board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
   let currentPlayer = 'X'
-  let playerX = []
-  let playerO = []
   let mode = 'human'
   let table = document.getElementsByTagName('TD')
 
- 
-  let button =document.getElementById('mode')
-  button.onclick = function toggleMode(){
-		if (button.innerHTML == 'Fight Skynet'){
-			mode = 'skynet'
-			button.innerHTML = 'Fight Human'
-		}	else{
-			mode = 'human'
-			button.innerHTML = 'Fight Skynet'
-		}
-	}
-  
-  function restartGame () {
-	document.getElementById('restart').onclick = function(){
-		initializeGame()
-	}
+  let button = document.getElementById('mode')
+  button.onclick = function toggleMode () {
+    if (button.innerHTML === 'Fight Skynet') {
+      mode = 'skynet'
+      button.innerHTML = 'Fight Human'
+    } else {
+      mode = 'human'
+      button.innerHTML = 'Fight Skynet'
+    }
   }
-  
+
+  function restartGame () {
+    document.getElementById('restart').onclick = function () {
+      initializeGame()
+    }
+  }
+
   function initializeGame () {
   }
 
   for (let i = 0; i < board.length; i++) {
     table[i].onclick = function (event) {
-      console.log(event.target + i)
+      boardStatus(event.target)
     }
   }
   function squareOpen (square) {
-    if (board[square] == 0) {return true}
-        return false
+    if (board[square.id] === 0) {return true}
+    return false
   }
 
-  function takeSquare (boardSquare, currentPlayerSymbol = 'X') {
-    board[sqaure] = currentPlayer // change to boardSquare id
+  function takeSquare (boardSquare) {
+    board[boardSqaure.id] = currentPlayer
     boardSquare.innerHTML = currentPlayer
-    if (currentPlayer == 'X'){ playerX.push[]}
-
+    if (currentPlayer === 'X') { currentPlayer = 'O'} else {currentPlayer = 'X'}
   }
   function skynet () {
     // min-max 
@@ -50,34 +46,28 @@ window.onload = function () {
     // if the board is full call results function maybe 
   }
   function gameStatus () {
-  	for(let i = 0; i <winningCombos.length; i ++){
-  		if(board[winningCombos[i][0]] == board[winningCombos[i][1]] == board[winningCombos[i][2]]){
+    for (let i = 0; i < winningCombos.length; i++) {
+      if (board[winningCombos[i][0]] == board[winningCombos[i][1]] == board[winningCombos[i][2]]) {
+      }
+    }
+    // use && instead of == maybe
+    // compare each three int array as index on the board to eachother and see if any are == 0
+    // if so break here
+    // then compare them to each other and see if they are the same
+    // if they are the same determine the person who has won and break the entire function 
+    // also end the ability to onclick= may need another variable for end of game
+    // otherwise cycle through the array of arrays
 
-  		}
-  	}
-  	//compare each three int array as index on the board to eachother and see if any are == 0
-  		//if so break here
-  	//then compare them to each other and see if they are the same
-  		//if they are the same determine the person who has won and break the entire function 
-  		//also end the ability to onclick= may need another variable for end of game
-  	//otherwise cycle through the array of arrays
-  
-
-  	}
   }
+
   function boardStatus (boardSquare) {
     if (squareOpen(boardSquare)) {
-      if (mode == 'computer') {
-        skynet()
-      } else if (currentPlayer === 'O' && mode == 'human') {
-        takeSquare(boardSquare)
-      } else {
-        takeSquare(boardSquare)
-      }
-    } // can these become promises or callbacks
+      takeSquare(boardSquare)
+    }
+    gameStatus()
+    if (mode === 'skynet') { skynet()}
   }
-  gameStatus()
-}
 
 // loop to add onclick handlers  I want to tie the dom elements to their class and status in the game without so many calls
 // to get ElementID and whatever, look at people who did not use canvas and see what they did
+}
