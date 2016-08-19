2330d5c090ad6dd81616763d39b885903ccd6d5a
Nicholas <nohman20@gmail.com>
2016-08-19 20:32:04 +0000

    Adds server, readme, package.json

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: [
+    './index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [
+      {test: /\.js|\.jsx$/, exclude: /node_modules/, loader: 'babel-loader'},
+      { test: /\.css$/, loader: 'style-loader!css-loader' }
+    ]
+  },
+  plugins: [HTMLWebpackPluginConfig]
+}
